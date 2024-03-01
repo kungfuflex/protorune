f6235bb99ad2933b2c6c054d6de92b1b4e67a419
Sandshrew <sandshrewmetaprotocols@gmail.com>
2024-03-01 00:50:27 +0000

    index range in test

--- tests/release.spec.ts
+++ tests/release.spec.ts
@@ -4,8 +4,8 @@ import { EventEmitter } from "events";
 import { IndexerProgram } from "metashrew-test";
 import path from "path";
 
-
 describe("metashrew index", () => {
+	/*
   it("indexes the genesis block", async () => {
     const program = new IndexerProgram(
       new Uint8Array(
@@ -21,7 +21,7 @@ describe("metashrew index", () => {
     );
     program.setBlockHeight(0);
     program.on("log", (v) => console.log(v));
-    await program.run('_start');
+    await program.run("_start");
     // console.log(program.kv);
   });
   it("indexes the first inscription", async () => {
@@ -39,7 +39,7 @@ describe("metashrew index", () => {
     );
     program.setBlockHeight(767430);
     program.on("log", (v) => console.log(v));
-    const ms = await program.run('_start');
+    const ms = await program.run("_start");
     // console.log(program.kv);
     // console.log(String(ms) + "ms");
   });
@@ -58,7 +58,7 @@ describe("metashrew index", () => {
     );
     program.setBlockHeight(785391);
     program.on("log", (v) => console.log(v));
-    const ms = await program.run('_start');
+    const ms = await program.run("_start");
     // console.log(program.kv);
     // console.log(String(ms) + "ms");
   });
@@ -76,11 +76,50 @@ describe("metashrew index", () => {
       await fs.readFile(path.join(__dirname, "772904.hex"), "utf8"),
     );
     program.setBlockHeight(772904);
-    program.on("log", (v) => console.log(v));
-    const ms = await program.run('_start');
+    prograom.on("log", (v) => console.log(v));
+    const mos = await program.run("_start");
     // console.log(program.kv);
     // console.log(String(ms) + "ms");
   });
-
-    
+  */
+  it("indexes a range of blocks", async () => {
+    const program = new IndexerProgram(
+      new Uint8Array(
+        Array.from(
+          await fs.readFile(
+            path.join(__dirname, "..", "build", "release.wasm"),
+          ),
+        ),
+      ).buffer,
+    );
+    program.on("log", (v) => console.log(v));
+    async function rpcCall(method, params) {
+      const response = await fetch(
+        "https://testnet.sandshrew.io/v1/154f9aaa25a986241357836c37f8d71",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            method,
+            params,
+            jsonrpc: "2.0",
+            id: Date.now(),
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        },
+      );
+      return (await response.json()).result;
+    }
+    async function runBlock(i: number) {
+      program.setBlock(
+        await rpcCall("getblock", [await rpcCall("getblockhash", [i]), 0]),
+      );
+      program.setBlockHeight(i);
+      await program.run("_start");
+    }
+    for (let i = 1000000; i < 1000050; i++) {
+      await runBlock(i);
+    }
+  });
 });
