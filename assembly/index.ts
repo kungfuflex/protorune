5b1d232cfe08a6a90abc5608a6c32c766e2c95bf
protoeloka <oobiora22@gmail.com>
2024-03-10 23:21:50 -0400

    update

--- assembly/index.ts
+++ assembly/index.ts
@@ -156,126 +156,6 @@ class Index {
     }
   }
 
-  static indexRanges(height: u32, block: Block): void {
-    let h = new Height(height);
-
-    let coinbase_inputs = new Array<Array<u64>>();
-
-    // if block subsidy is greater than 0, add range to coinbase input
-    if (h.subsidy() > 0) {
-      let start: Sat = h.startingSat();
-      coinbase_inputs.push([<u64>(start.n()), <u64>(start.n() + h.subsidy())]);
-    }
-
-    // iterate over transaction excluding coinbase
-    for (let tx_offset = 1; tx_offset < block.transactions.length; tx_offset++) {
-
-      let tx = block.transactions[tx_offset];
-      let inputOrdinalsRange = new Array<Array<u64>>();
-
-      // transaction inputs
-      for (let i = 0; i < tx.ins.length; i++) {
-        let input = tx.ins[i];
-        // encode key from transaction outpoint
-        let key: ArrayBuffer = input.previousOutput().toBuffer();
-
-        let response = get(Index.keyFor(OUTPOINT_TO_ORDINALS_RANGE, key));
-        let ordinalRanges = Box.from(response);
-        let numOfOrdinals = parsePrimitive<u32>(ordinalRanges);
-        for ( let i = 0; i < <i32>numOfOrdinals; i++ ) {
-          let start = parsePrimitive<u64>(ordinalRanges);
-          let end = parsePrimitive<u64>(ordinalRanges);
-          inputOrdinalsRange.push([start, end])
-        }
-      }
-
-      // transaction outputs
-      for (let i = 0; i < tx.outs.length; i++) {
-
-        let ordinals: Array<ArrayBuffer> = [];
-        let output = tx.outs[i];
-
-        let key = OutPoint.from(tx.txid(), i).toBuffer();
-
-        let remaining = output.value;
-        while (remaining > 0) {
-          if (inputOrdinalsRange.length == 0) {
-            break;
-          } 
-
-          let range = inputOrdinalsRange.shift();
-          let count = range[1] - range[0];
-          let assigned: Array<u64> = new Array();
-
-          if (count > remaining) {
-            let middle = range[0] + remaining;
-            inputOrdinalsRange.unshift([middle, range[1]]);
-            assigned = [range[0], middle];
-          } else {
-            assigned = range;
-          }
-
-          let start = primitiveToBuffer<u64>(assigned[0]);
-          let end = primitiveToBuffer<u64>(assigned[1]);
-          let ordinalRange = concat([start, end]);
-          ordinals.push(ordinalRange);
-          remaining -= assigned[1] - assigned[0];
-        }
-
-        let numOrdinals = primitiveToBuffer<u32>(<u32>ordinals.length)
-        ordinals.unshift(numOrdinals);
-        let ords = concat(ordinals);
-        set(Index.keyFor(OUTPOINT_TO_ORDINALS_RANGE, key), ords); 
-      }
-
-      for (let i = 0; i < inputOrdinalsRange.length; i++){
-        coinbase_inputs.push(inputOrdinalsRange[i]);
-      }
-    }
-
-    // coinbase transaction 
-    // handle if coinbase is null error
-    let coinbase = block.coinbase();
-    if (coinbase == null) {
-      return;
-    }
-
-    for (let vout = 0; vout < coinbase.outs.length; vout++) {
-      let output = coinbase.outs[vout];
-      let ordinals: Array<ArrayBuffer> = [];
-      let key = OutPoint.from(coinbase.txid(), <u32>vout).toBuffer();
-      let remaining = output.value;
-
-      while (remaining > 0) {
-        if (coinbase_inputs.length == 0) {
-          break;
-        }
-        let range = coinbase_inputs.shift();
-        let count = range[1] - range[0];
-        let assigned: Array<u64> = new Array();
-        if (count > remaining) {
-          let middle = range[0] + remaining;
-          coinbase_inputs.unshift([middle, range[1]]);
-          assigned = [range[0], middle];
-        } else {
-          assigned = range;
-        }
-
-        let start = primitiveToBuffer<u64>(assigned[0]);
-        let end = primitiveToBuffer<u64>(assigned[1]);
-        let ordinalRange = concat([start, end]);
-        ordinals.push(ordinalRange);
-
-        remaining -= assigned[1] - assigned[0];
-      }
-      
-      
-      let numOrdinals = primitiveToBuffer<u32>(<u32>ordinals.length)
-      ordinals.unshift(numOrdinals);
-      let ords = concat(ordinals);
-      set(Index.keyFor(OUTPOINT_TO_ORDINALS_RANGE, key), ords); 
-    }
-  }
 }
 
 
@@ -284,7 +164,6 @@ export function _start(): void {
   const box = Box.from(data);
   const height = parsePrimitive<u32>(box);
   const block = new Block(box);
-  // Index.indexRanges(height, block);
   Index.indexBlock(height, block);
   _flush();
 }
