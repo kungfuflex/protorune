6bbf1947a0766fd6bfc60feb98bc8dd0a3ac5828
hudeintres <nodadfat@gmail.com>
2024-07-09 04:25:51 +0000

    rm unused runes stuff

--- lib/wallet.d.ts
+++
@@ -1,6 +0,0 @@
-import { OutPoint, RuneOutput } from "./outpoint";
-export declare function decodeWalletOutput(hex: string): {
-    outpoints: OutPoint[];
-    balanceSheet: RuneOutput[];
-};
-export declare function encodeWalletInput(address: string): string;
