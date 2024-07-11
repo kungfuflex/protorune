0961b4529bd94a3e3eac13791255e2e0adba4b9c
v16s <16210686+v16s@users.noreply.github.com>
2024-07-11 15:23:02 +0530

    add protomessage encoding in ts lib

--- src.ts/protostone.ts
+++ src.ts/protostone.ts
@@ -12,7 +12,12 @@ export type ProtoBurn = {
   pointer: Option<u32>;
 };
 
-export type ProtoMesage = {};
+export type ProtoMessage = {
+  calldata: u128[];
+  pointer: Option<u32>;
+  refundPointer: Option<u32>;
+  protocolTag: u128;
+};
 
 export type ProtoSplit = {};
 
@@ -20,19 +25,46 @@ export type Chunk = {};
 
 export class ProtoStone {
   burn?: ProtoBurn;
+  message?: ProtoMessage;
+  split?: ProtoSplit;
 
   constructor({
     burn,
+    message,
   }: {
     burn?: { protocolTag: bigint; pointer: number };
-    chunk?: {};
-    message?: {};
-    split?: {};
+    message?: {
+      calldata: Buffer;
+      protocolTag: bigint;
+      pointer: number;
+      refundPointer: number;
+    };
   }) {
     if (burn) {
       this.burn = {
         protocolTag: Some<u128>(u128(burn.protocolTag)),
         pointer: Some<u32>(u32(burn.pointer)),
+      };
+    }
+    if (message) {
+      const ary = Uint8Array.from(message.calldata);
+      const res: u128[] = [];
+
+      for (let i = 0; i < ary.byteLength; i += 8) {
+        const last = i + 8;
+        res.push(
+          u128(
+            BigInt(
+              `0x${Buffer.from(ary.slice(i, last > ary.byteLength ? ary.byteLength : last)).toString("hex")}`,
+            ),
+          ),
+        );
+      }
+      this.message = {
+        calldata: res,
+        protocolTag: u128(message.protocolTag),
+        pointer: Some<u32>(u32(message.pointer)),
+        refundPointer: Some<u32>(u32(message.refundPointer)),
       };
     }
   }
@@ -49,6 +81,16 @@ export class ProtoStone {
         Tag.encodeOptionInt(Tag.BURN, this.burn.protocolTag.map(u128)),
       );
       stack.push(opcodes.OP_14);
+    } else if (this.message) {
+      payloads.push(u128.encodeVarInt(this.message.protocolTag));
+      payloads.push(
+        Tag.encodeOptionInt(Tag.POINTER, this.message.pointer.map(u128)),
+      );
+      payloads.push(
+        Tag.encodeOptionInt(Tag.REFUND, this.message.refundPointer.map(u128)),
+      );
+      payloads.push(Tag.encode(Tag.MESSAGE, this.message.calldata));
+      stack.push(opcodes.OP_16);
     }
     const payload = Buffer.concat(payloads);
     for (let i = 0; i < payload.length; i += MAX_SCRIPT_ELEMENT_SIZE) {
@@ -60,5 +102,14 @@ export class ProtoStone {
 
   static burn(burn: { protocolTag: bigint; pointer: number }): ProtoStone {
     return new ProtoStone({ burn });
+  }
+
+  static message(message: {
+    calldata: Buffer;
+    protocolTag: bigint;
+    pointer: number;
+    refundPointer: number;
+  }): ProtoStone {
+    return new ProtoStone({ message });
   }
 }
