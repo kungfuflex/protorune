2e1f8829260809888858d0d69a6d7507265e7142
hudeintres <nodadfat@gmail.com>
2024-07-26 03:02:47 +0000

    add test with pointer

--- tests/protoburn.spec.ts
+++ tests/protoburn.spec.ts
@@ -704,4 +704,126 @@ describe("protoburns", () => {
       "address 2 should now have 2*amount",
     );
   });
+  it("multiple edicts will cycle around all protoburns including pointer", async () => {
+    const program = buildProgram(DEBUG_WASM);
+    program.setBlockHeight(840000);
+    const premineAmount = 2100000005000000n;
+    const outputs = [
+      {
+        script: bitcoinjs.payments.p2pkh({
+          address: TEST_BTC_ADDRESS1,
+          network: bitcoinjs.networks.bitcoin,
+        }).output,
+        value: 1,
+      },
+      {
+        script: bitcoinjs.payments.p2pkh({
+          network: bitcoinjs.networks.bitcoin,
+          address: TEST_BTC_ADDRESS2,
+        }).output,
+        value: 624999999,
+      },
+    ];
+    const pointer1 = 1;
+    let block = initCompleteBlockWithRuneEtching(
+      outputs,
+      pointer1,
+      undefined,
+      premineAmount,
+    );
+
+    const input = {
+      inputTxHash: block.transactions?.at(1)?.getHash(), // 0 is coinbase, 1 is the mint
+      inputTxOutputIndex: pointer1, // index of output in the input tx that has the runes. In this case it is the default pointer of the mint
+    };
+    const runeId = {
+      block: 840000n,
+      tx: 1,
+    };
+    const amount = premineAmount / 4n;
+    const output = {
+      address: TEST_BTC_ADDRESS2,
+      btcAmount: 1, //this can be implied to be 1 since runes usually are just inscribed on a satoshi
+    };
+    // technically this is not a valid transaction since btc in and less than btc out but this is just to test the runes
+    const refundOutput = {
+      address: TEST_BTC_ADDRESS1,
+      btcAmount: 1, // this gives address 1 his remaining bitcoin
+    };
+
+    // output 0: runestone with protoburns
+    // output 1-2: output, and refundOutput
+    // This transaction does a protoburn and transfers all protorunes to output 2
+    block = constructProtostoneTx(
+      [input],
+      [output, refundOutput], // 0 is script, 1 is address 2 output, 2 is address 1 output
+      [
+        {
+          id: runeId,
+          amount: amount,
+          output: 0, // this goes to protoburn 1, which goes to ADDRESS2
+        },
+        {
+          id: runeId,
+          amount: amount,
+          output: 0, // this goes to protoburn 2
+        },
+        {
+          id: runeId,
+          amount: amount,
+          output: 0, // this goes to protoburn 1
+        },
+      ],
+      [
+        // protoburn 1
+        ProtoStone.burn({
+          protocolTag: TEST_PROTOCOL_TAG,
+          pointer: 1,
+        }),
+        //protoburn 2
+        ProtoStone.burn({
+          protocolTag: TEST_PROTOCOL_TAG,
+          pointer: 2,
+        }),
+      ],
+      block,
+      /*runeTransferPointer=*/ 0,
+    );
+
+    program.setBlock(block.toHex());
+
+    await program.run("_start");
+
+    const resultAddress1 = await runesbyaddress(program, TEST_BTC_ADDRESS1);
+    expect(resultAddress1.balanceSheet.length).equals(
+      0,
+      "address 1 should not have any runes left",
+    );
+    const resultAddress2 = await runesbyaddress(program, TEST_BTC_ADDRESS2);
+    expect(resultAddress2.balanceSheet.length).equals(
+      0,
+      "address 2 should not have received any runes",
+    );
+    const protorunesAddress1 = await protorunesbyaddress(
+      program,
+      TEST_BTC_ADDRESS1,
+      TEST_PROTOCOL_TAG,
+    );
+    //console.log(protorunesAddress1);
+    expect(protorunesAddress1.balanceSheet[0].balance).equals(
+      2n * amount,
+      "address 1 should now have amount from first edict and third edict",
+    );
+
+    const protorunesAddress2 = await protorunesbyaddress(
+      program,
+      TEST_BTC_ADDRESS2,
+      TEST_PROTOCOL_TAG,
+    );
+    //console.log(protorunesAddress2);
+    expect(protorunesAddress2.balanceSheet[0].balance).equals(
+      2n * amount,
+      "address 2 should now have amount from second edict and pointer",
+    );
+  });
 });
