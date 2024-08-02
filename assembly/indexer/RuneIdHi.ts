d508dca6ddc8329e0653698cd217d4737bef3cd7
v16s <16210686+v16s@users.noreply.github.com>
2024-08-02 17:01:30 +0530

    add RuneId which encodes onto u128 hi

---
+++ assembly/indexer/RuneIdHi.ts
@@ -0,0 +1,18 @@
+import { u128 } from "as-bignum/assembly";
+import { RuneId as RuneIdBase } from "metashrew-runes/assembly/indexer/RuneId";
+
+export class RuneIdHi extends RuneIdBase {
+  toBytes(): ArrayBuffer {
+    let bytes = this.block.toBytes(true);
+    bytes = bytes.concat(this.tx.toBytes(true));
+    return changetype<Uint8Array>(bytes).buffer;
+  }
+
+  static fromBytes(ary: ArrayBuffer): RuneIdHi {
+    const parsed = changetype<Array<u8>>(Uint8Array.wrap(ary));
+    const block = u128.fromBytes(parsed.slice(0, 16), true).toU64();
+    const tx = u128.fromBytes(parsed.slice(16, 32), true).toU32();
+
+    return new RuneIdHi(block, tx);
+  }
+}
