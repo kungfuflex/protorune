1f8112db9588c67ed5b23d5b40e678b75feb35e8
kungfuflex <judoflexchop@gmail.com>
2024-09-18 19:43:11 +0000

    update

--- assembly/index-regtest.ts
+++ assembly/index-regtest.ts
@@ -6,8 +6,10 @@ import { DefaultProtorune, Protorune } from "./indexer";
 import { MessageContext } from "./indexer/protomessage";
 import { GENESIS } from "metashrew-runes/assembly/indexer/constants";
 import { SpendablesIndex } from "metashrew-spendables/assembly/indexer";
+import { network } from "metashrew-as/assembly/blockdata/network";
 
 export function _start(): void {
+  network.setNetwork(network.Network.REGTEST);
   const data = input();
   const box = Box.from(data);
   const height = parsePrimitive<u32>(box);
